00ea2634c402cacb0c9c6d9f0d9a294617aba0af
Sca1ey <tim@thurgoland.net>
2026-02-24 16:58:13 +0000

    added button grid

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 // wxWidgets "Hello World" Program
- 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
  
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
- 
+
+// Create the app 
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
- 
+
+// Declare the Frame Class, constructor and event handlers
 class MyFrame : public wxFrame
 {
 public:
@@ -23,12 +24,14 @@ private:
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 };
- 
+
+// Declare an ID for the Reset event
 enum
 {
-    ID_Hello = 1
+   ID_Reset = 1
 };
- 
+
+// Initialise the App and call MyFrame 
 wxIMPLEMENT_APP(MyApp);
  
 bool MyApp::OnInit()
@@ -37,33 +40,54 @@ bool MyApp::OnInit()
     frame->Show(true);
     return true;
 }
- 
+
+// Define the contents of the MyFrame in its constructor
 MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, "Hello World")
+    : wxFrame(NULL, wxID_ANY, "Noughts and Crosses",wxDefaultPosition,wxSize(450,400))
 {
+    //File menu
     wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
+    menuFile->Append(ID_Reset, "&Reset\tCtrl-R",
+                     "Reset the game!");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
  
+    //Help Menu
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
- 
+        
+    //Add menus to menu bar
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append(menuFile, "&File");
     menuBar->Append(menuHelp, "&Help");
  
     SetMenuBar( menuBar );
  
+    //Status bar
     CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    SetStatusText("Welcome to Noughts and Crosses.");
+
+    //Bind event handlers for menu 
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Reset);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+    //Add Button Grid
+    wxGridSizer *grid = new wxGridSizer(3,1,1);
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+    grid->Add(new wxButton(this,wxID_ANY,""),wxSizerFlags().Align(wxALIGN_CENTER));
+        
+    this->SetSizer(grid);
 }
- 
+
+// Define Event handlers 
 void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
@@ -71,11 +95,12 @@ void MyFrame::OnExit(wxCommandEvent& event)
  
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox("This is a wxWidgets version of Noughts and Crosses",
+                 "About Noughts and Crosses", wxOK | wxICON_INFORMATION);
 }
  
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    wxLogMessage("TODO: Reset the Game!");
 }
+
